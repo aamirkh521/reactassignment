c973f740979fb7b35cdb319e3fcf6f51f26d8d3d
aamirkh521 <mraamir8@gmail.com>
2020-09-09 22:50:55 +0530

    Updates

---
+++ precache-manifest.cf0607251303e05b02ae8a31f8871ce7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8835d659c4d712f69fb108f1f4187ff",
+    "url": "/github.io/reactassignment/index.html"
+  },
+  {
+    "revision": "114655d07d5d1ede87a2",
+    "url": "/github.io/reactassignment/static/css/main.e5857c85.chunk.css"
+  },
+  {
+    "revision": "5d706a4676ce1ac291ae",
+    "url": "/github.io/reactassignment/static/js/2.3e10ef85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/reactassignment/static/js/2.3e10ef85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "114655d07d5d1ede87a2",
+    "url": "/github.io/reactassignment/static/js/main.04231a8e.chunk.js"
+  },
+  {
+    "revision": "f735a154c448e8b7a9e5",
+    "url": "/github.io/reactassignment/static/js/runtime-main.8b856048.js"
+  }
+]);
